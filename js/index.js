0603309adb01cf862dbf065860a8536d55a4b011
tangyul <1906568584@qq.com>
2021-11-13 14:35:25 +0800

    这是第六周的项目文件

--- js/index.js
+++ js/index.js
@@ -63,4 +63,29 @@ $('#clother-banner').tyslide({
   controlsColor:"#d7d7d7",//普通控制按钮的颜色
   controlsCurrentColor:"#00ff00",//当前控制按钮的颜色
 });
+/*服装轮播 */
+$('#sport-banner').tyslide({
+  boxh:334,//盒子的高度
+  w:482,//盒子的宽度
+  h:334,//图片的高度
+  isShow:true,//是否显示控制器
+  isShowBtn:true,//是否显示左右按钮
+  controltop:10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+  controlsW:20,//控制按钮宽度
+  controlsH:2,//控制按钮高度
+  controlsColor:"#d7d7d7",//普通控制按钮的颜色
+  controlsCurrentColor:"#00ff00",//当前控制按钮的颜色
+});
+$('#children-clothes-banner').tyslide({
+  boxh:334,//盒子的高度
+  w:482,//盒子的宽度
+  h:334,//图片的高度
+  isShow:true,//是否显示控制器
+  isShowBtn:true,//是否显示左右按钮
+  controltop:10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+  controlsW:20,//控制按钮宽度
+  controlsH:2,//控制按钮高度
+  controlsColor:"#d7d7d7",//普通控制按钮的颜色
+  controlsCurrentColor:"#00ff00",//当前控制按钮的颜色
+});
 })
