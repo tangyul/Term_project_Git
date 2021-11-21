57e0f875b65ad73ff95a7a892a5a22d1e610b56e
tangyul <1906568584@qq.com>
2021-11-21 16:21:32 +0800

    这是第六周的文件

--- js/index.js
+++ js/index.js
@@ -33,23 +33,27 @@ $('#ebooks-banner').tyslide({
   controlsCurrentColor:"#00ff00",//当前控制按钮的颜色
 });
  /*电子书 table切换*/
- var $lis = $('.ebooks .ebooks-nav >li');
- $lis.mouseover(function() {
-         //给自己添加激活类active 把兄弟的都干掉
-         $(this).addClass('active').siblings().removeClass('active');
-         //获取index
-         var index = $(this).index();
-         //选中内容
-         var $ebooksList = $('.ebooks-list');
-         //显示索引等于当前index
-         $ebooksList.eq(index).show().siblings('.ebooks-list').hide();
-     })
+ $('.ebooks-nav > li').mouseenter(function(){
+   //导航高亮颜色切换
+   $(this).addClass('active').siblings('li').removeClass('active');
+   //获取索引
+   var index=$(this).index();
+   //内容切换
+   $('.ebooks-list').eq(index).show().siblings('.ebooks-list').hide;
+ });
+
  /*新书列表手风琴效果 */
-$('.ebooks .right-box ul > li').mouseenter(function(){
-  //所有兄弟：隐藏详情 显示标题
-  $(this).siblings().find('.desc').hide();
-  $(this).siblings().find('.ebooks-title').show();
-});
+ $('.ebook .right-box ul > li').mouseenter(function(){
+   //所有兄弟，详情隐藏，标题显示
+   $(this).siblings().find('.desc').hide()
+   $(this).siblings().find('.ebooks-title').show();
+
+   //当前，隐藏标题，显示详情
+   $(this).find('.ebooks-title').hide();
+   $(this).find('.desc').show();
+
+ });
+ 
 /*服装轮播 */
 $('#clother-banner').tyslide({
   boxh:334,//盒子的高度
@@ -63,7 +67,16 @@ $('#clother-banner').tyslide({
   controlsColor:"#d7d7d7",//普通控制按钮的颜色
   controlsCurrentColor:"#00ff00",//当前控制按钮的颜色
 });
-/*服装轮播 */
+/*服装 table切换*/
+$('.clothes .clothes-nav > li').mouseenter(function(){
+  //导航高亮颜色切换
+  $(this).addClass('active').siblings('li').removeClass('active');
+  //获取索引
+  var index=$(this).index();
+  //内容切换
+  $('.rightbox').eq(index).show().siblings('.rightbox').hide;
+});
+/*户外轮播 */
 $('#sport-banner').tyslide({
   boxh:334,//盒子的高度
   w:482,//盒子的宽度
@@ -76,6 +89,16 @@ $('#sport-banner').tyslide({
   controlsColor:"#d7d7d7",//普通控制按钮的颜色
   controlsCurrentColor:"#00ff00",//当前控制按钮的颜色
 });
+/*户外 table切换*/
+$('.sport .sport-nav > li').mouseenter(function(){
+  //导航高亮颜色切换
+  $(this).addClass('active').siblings('li').removeClass('active');
+  //获取索引
+  var index=$(this).index();
+  //内容切换
+  $('.rightbox').eq(index).show().siblings('.rightbox').hide;
+});
+/*童装轮播 */
 $('#children-clothes-banner').tyslide({
   boxh:334,//盒子的高度
   w:482,//盒子的宽度
@@ -88,4 +111,53 @@ $('#children-clothes-banner').tyslide({
   controlsColor:"#d7d7d7",//普通控制按钮的颜色
   controlsCurrentColor:"#00ff00",//当前控制按钮的颜色
 });
+/*童装 table切换*/
+$('.children-clothes .children-nav > li').mouseenter(function(){
+  //导航高亮颜色切换
+  $(this).addClass('active').siblings('li').removeClass('active');
+  //获取索引
+  var index=$(this).index();
+  //内容切换
+  $('.rightbox').eq(index).show().siblings('.rightbox').hide;
+});
+
+/*推广商品切换 */
+$('.promotion .roof ul li').mouseenter(function(){
+  //导航激活类的切换
+  $(this).addClass('active').siblings().removeClass('active')
+ 
+  //内容切换
+  //获取对应索引
+  var index = $(this).index();
+  console.log(index)
+  console.log($('.promotion .main .inner-box'))
+  //左右移动
+  $('.promotion .main .inner-box').animate({
+    'left': -index * 1170
+  },300)
+})
+
+/*推广商品主图变色
+$('.promotion .main .promotion-list li').mouseenter(function(){
+  $('.promotion .main .promotion-list li').css("background","#ccc");
+})*/
+
+
+/*返回顶部 */
+$(document).scroll(function(){
+  //获取距离顶部的位置
+  var topDistance = $('html,body').scrollTop();
+  //判断
+  if( topDistance > 500 ){
+      $('.backToTop').fadeIn();
+  }else{
+      $('.backToTop').fadeOut();
+  }
+})
+//返回顶部
+$('.backToTop').click(function(){
+  $('html,body').animate({
+    scrollTop:0
+  },300)
+})
 })
